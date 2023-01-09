b0800874ee2e7266cb8977caa1c7b52d327733c8
Ctasi <cwxtasi@163.com>
2023-01-09 23:20:35 +0800

    爬虫修改

--- akshare/fund/fund_em_manager.py
+++ akshare/fund/fund_em_manager.py
@@ -25,8 +25,19 @@ def fund_manager_fund_info_em(fund: str = "000009") -> pd.DataFrame:
     url = f"http://fundf10.eastmoney.com/jjjl_{fund}.html"
     print(url)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
-        "Referer": "http://fundf10.eastmoney.com/"
+        # "Accept": "*/*",
+        # "Accept-Encoding": "gzip, deflate, br",
+        # "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "Connection": "keep-alive",
+        # "Host": "sso.1234567.com.cn",
+        "Referer": "http://fundf10.eastmoney.com/",
+        # "sec-ch-ua": "\"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"108\", \"Microsoft Edge\";v=\"108\"",
+        # "sec-ch-ua-mobile": "?0",
+        # "sec-ch-ua-platform": "\"Windows\"",
+        # "Sec-Fetch-Dest": "script",
+        "Sec-Fetch-Mode": "no-cors",
+        "Sec-Fetch-Site": "cross-site",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76"
     }
     r = requests.get(url, headers=headers)
     html = etree.HTML(r.text)
@@ -42,21 +53,42 @@ def fund_manager_fund_info_em(fund: str = "000009") -> pd.DataFrame:
         return_data = html.xpath('//table[@class="w782 comm  jloff"]/tbody/tr[{}]/td[5]/text()'.format(row))
         tz = pytz.timezone('Asia/Shanghai')  # 东八区
         t = datetime.datetime.fromtimestamp(int(time.time()), tz).strftime('%Y-%m-%d %H:%M:%S %z')
+        if end_time[0] == '至今':
+            end_time[0] = None
+
         for text in manager:
             res_tr = []
             res_tr.append(start_time[0])
-            res_tr.append(end_time[0])
+
+            if len(end_time) > 0:
+                res_tr.append(end_time[0])
+            else:
+                res_tr.append(None)
+
             res_tr.append(text.text)
-            res_tr.append(text.attrib['href'].replace('http://fund.eastmoney.com/manager/', '').replace('.html', ''))
-            res_tr.append(total_time[0])
-            res_tr.append(return_data[0])
+            try:
+                res_tr.append(text.attrib['href'].replace('http://fund.eastmoney.com/manager/', '').replace('.html', ''))
+            except Exception as e:
+                res_tr.append('')
+
+            if len(total_time) > 0:
+                res_tr.append(total_time[0])
+            else:
+                res_tr.append(0)
+
+            if len(return_data) > 0:
+                res_tr.append(return_data[0].replace('%',''))
+            else:
+                res_tr.append(0)
+
             res_tr.append(t)
             res_tr.append(t)
+            res_tr.append(fund)
             result.append(res_tr)
         row += 1
 
     data = pd.DataFrame(result,columns=['start_time', 'end_time', 'manager', 'manager_id', 'total_time', 'total_return',
-                                 'dw_insert_time', 'dw_update_time'])
+                                 'dw_insert_time', 'dw_update_time','ts_code'])
 
     return data
 
@@ -91,7 +123,7 @@ def fund_manager_det_fund_info_em(fund: str = "30532593") -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    fund_money_fund_info_em_df = fund_manager_fund_info_em(fund="000001")
+    fund_money_fund_info_em_df = fund_manager_fund_info_em(fund="002005")
     print(fund_money_fund_info_em_df)
     # fund_manager_det_fund_info_em = fund_manager_det_fund_info_em(fund="30532593")
     # print(fund_manager_det_fund_info_em)
